7f1d5dfa785c67355019e0669e5351a83da52946
traxys <quentin+dev@familleboyer.net>
2024-03-28 23:10:31 +0100

    Initial oven handling

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,132 @@
-fn main() {}
+use axum::{routing::post, Json, Router};
+use time::OffsetDateTime;
+use tower_http::trace::TraceLayer;
+use tracing::Level;
+use tracing_subscriber::EnvFilter;
+use url::Url;
+
+#[derive(serde::Deserialize, Debug)]
+pub struct OvenClient {
+    pub address: String,
+    pub port: u16,
+    pub user_agent: String,
+}
+
+#[derive(serde::Deserialize, Debug)]
+#[serde(rename_all = "lowercase")]
+pub enum OvenDirection {
+    Incoming,
+    Outgoing,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub enum OvenProtocol {
+    WebRTC,
+    RTMP,
+    SRT,
+    LLHLS,
+    Thumbnail,
+}
+
+#[derive(serde::Deserialize, Debug)]
+#[serde(rename_all = "lowercase")]
+pub enum OvenStatus {
+    Closing,
+    Opening,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct OvenRequest {
+    pub direction: OvenDirection,
+    pub protocol: OvenProtocol,
+    pub status: OvenStatus,
+    pub url: Url,
+    pub new_url: Option<Url>,
+    #[serde(deserialize_with = "time::serde::iso8601::deserialize")]
+    pub time: OffsetDateTime,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct OvenAdmission {
+    pub client: OvenClient,
+    pub request: OvenRequest,
+}
+
+#[derive(serde::Serialize, Debug)]
+pub struct OvenClosingResponse {}
+
+#[derive(serde::Serialize, Debug)]
+pub struct OvenOpeningResponse {
+    pub allowed: bool,
+    pub new_url: Option<Url>,
+    pub lifetime: Option<u64>,
+    pub reason: Option<String>,
+}
+
+#[derive(serde::Serialize, Debug)]
+#[serde(untagged)]
+pub enum OvenResponse {
+    Closing(OvenClosingResponse),
+    Opening(OvenOpeningResponse),
+}
+
+impl From<OvenOpeningResponse> for Json<OvenResponse> {
+    fn from(value: OvenOpeningResponse) -> Self {
+        Json(OvenResponse::Opening(value))
+    }
+}
+
+impl From<OvenClosingResponse> for Json<OvenResponse> {
+    fn from(value: OvenClosingResponse) -> Self {
+        Json(OvenResponse::Closing(value))
+    }
+}
+
+fn handle_opening_admission(_payload: OvenAdmission) -> anyhow::Result<OvenOpeningResponse> {
+    Ok(OvenOpeningResponse {
+        allowed: true,
+        lifetime: None,
+        new_url: None,
+        reason: None,
+    })
+}
+
+#[tracing::instrument]
+async fn admission(payload: Json<OvenAdmission>) -> Json<OvenResponse> {
+    tracing::trace!("Received admission request");
+
+    match payload.request.status {
+        OvenStatus::Closing => OvenClosingResponse {}.into(),
+        OvenStatus::Opening => match handle_opening_admission(payload.0) {
+            Err(err) => OvenOpeningResponse {
+                allowed: false,
+                new_url: None,
+                lifetime: None,
+                reason: Some(err.to_string()),
+            },
+            Ok(rsp) => rsp,
+        }
+        .into(),
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(Level::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+
+    let app = Router::new()
+        .route("/oven/admission", post(admission))
+        .layer(TraceLayer::new_for_http());
+
+    let listener = tokio::net::TcpListener::bind(("0.0.0.0", 3000)).await?;
+
+    tracing::info!("Starting oven-ctrl");
+
+    axum::serve(listener, app).await.map_err(Into::into)
+}
